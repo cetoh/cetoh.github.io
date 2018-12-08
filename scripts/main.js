d4388a2dcd0fc2f4458e16c8eb5e63623c2fa886
Chris Toh <31299566+cetoh@users.noreply.github.com>
2018-12-07 23:12:47 -0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 var myImage = document.getElementById("profile");
 
-myImage.onclick = function() {
-    if(myImage.getAttribute('src') == "\images\Senior-55.jpg") {
-      	myImage.src = "\images\Senior-115.jpg";
+myImage.onclick = function pictureChange() {
+    if(myImage.getAttribute('src') == "Senior-55.jpg") {
+      	myImage.src = "Senior-115.jpg";
     } else {
-      	myImage.src = "\images\Senior-55.jpg";
+      	myImage.src = "Senior-55.jpg";
     }
 }

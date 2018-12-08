90bab72686a8abb3e0774cda34db8a819c0232f2
Chris Toh <31299566+cetoh@users.noreply.github.com>
2018-12-07 21:54:34 -0800

    Added Script to switch pictures on click

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/Senior-55.jpg') {
+      myImage.setAttribute ('src','images/Senior-155.jpg');
+    } else {
+      myImage.setAttribute ('src','images/Senior-55.jpg');
+    }
+}

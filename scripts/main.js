184174f76ec74e0da058161cab39ab74b1cc8923
Chris Toh <31299566+cetoh@users.noreply.github.com>
2018-12-07 22:53:33 -0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,8 @@ var myImage = document.querySelector('img');
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
     if(mySrc === 'images\Senior-55.jpg') {
-      myImage.setAttribute ('src','images\Senior-115.jpg');
+      	myImage.setAttribute('src','\images\Senior-115.jpg');
     } else {
-      myImage.setAttribute ('src','images\Senior-55.jpg');
+      	myImage.setAttribute('src','\images\Senior-55.jpg');
     }
 }

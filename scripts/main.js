bca65e2ee6b78a1bacb90d01eeabaf0b526a0724
Chris Toh <31299566+cetoh@users.noreply.github.com>
2018-12-07 22:06:03 -0800

    Fixed filename

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ var myImage = document.querySelector('img');
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/Senior-55.jpg') {
-      myImage.setAttribute ('src','images/Senior-155.jpg');
+      myImage.setAttribute ('src','images/Senior-115.jpg');
     } else {
       myImage.setAttribute ('src','images/Senior-55.jpg');
     }
